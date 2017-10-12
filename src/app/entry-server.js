b9805ab7a0fb2372e5c48a11dff7690924dcbe0a
songlairui <cnre@live.cn>
2017-10-13 00:18:00 +0800

    增加 leancloud-storage 初始化

--- src/app/entry-server.js
+++ src/app/entry-server.js
@@ -6,6 +6,18 @@ import 'material-design-icons/iconfont/material-icons.css'
 import 'muse-ui/dist/muse-ui.css'
 import 'muse-ui/dist/theme-teal.css' // 使用 carbon 主题
 
+import AV from 'leancloud-storage'
+
+const appId = 'YLjYRcnrFf8PyKSXAaaflf9L-gzGzoHsz'
+const appKey = 'VX5DqVUXWdwNjv6TUW8y9qte'
+
+AV.init({
+  appId,
+  appKey
+  // 启用美国节点
+  // region: 'us',
+})
+
 export default context => {
   // 使用promise，异步过程
   return new Promise((resolve, reject) => {
